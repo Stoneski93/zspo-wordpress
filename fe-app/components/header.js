a8d98a93a3cb9875eebb9445067c15c1d19daee1
Norbert Kamienski <norbert.kamienski@pagepro.pl>
2021-09-30 21:07:24 +0200

    add gitignore next and remove some stuff

--- fe-app/components/header.js
+++ fe-app/components/header.js
@@ -1,12 +1,39 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Container from "./container";
 
 export default function Header() {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/">
-        <a className="hover:underline">Blog</a>
-      </Link>
-      .
-    </h2>
-  )
+    <section>
+      <div className="bg-blue-50">
+        <Container>
+          <div className="flex-row flex justify-between ">
+            <h2 className="text-xl font-bold tracking-tight md:tracking-tighter leading-tight my-8 text-gray-800 ">
+              <Link href="/">
+                <a>
+                  Zespół Szkolno Przedszkolny
+                  <br /> w Ogrodniczkach
+                </a>
+              </Link>
+            </h2>
+            <h2 className="text-sm font-bold tracking-tight md:tracking-tighter leading-tight my-8 text-gray-800">
+              Sekretariat: (85) 651 21 74
+            </h2>
+          </div>
+        </Container>
+      </div>
+
+      <div>
+        <Container>
+          <ul className="flex-row flex justify-end items-center mt-8 mb-8">
+            <li className="mr-8 text-xl text-gray-800">Strona główna</li>
+            <li className="mr-8 text-xl text-gray-800">Szkoła</li>
+            <li className="mr-8 text-xl text-gray-800">Galeria</li>
+            <li className="mr-8 text-xl text-gray-800">Kontakt</li>
+            <li className="mr-8 text-xl text-gray-800">Ogłoszenia</li>
+            <li className="mr-8 text-xl text-gray-800">E-dziennik</li>
+          </ul>
+        </Container>
+      </div>
+    </section>
+  );
 }
--- fe-app/pages/index.js
+++ fe-app/pages/index.js
@@ -1,22 +1,24 @@
-import Head from 'next/head'
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
+import Head from "next/head";
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import { getAllPostsForHome } from "../lib/api";
+import { CMS_NAME } from "../lib/constants";
+import Header from "../components/header";
 
 export default function Index({ allPosts: { edges }, preview }) {
-  const heroPost = edges[0]?.node
-  const morePosts = edges.slice(1)
+  const heroPost = edges[0]?.node;
+  const morePosts = edges.slice(1);
 
   return (
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Zespół Szkolno Przedszkolny w Ogrodniczkach</title>
         </Head>
+        <Header />
         <Container>
           <Intro />
           {heroPost && (
@@ -33,12 +35,12 @@ export default function Index({ allPosts: { edges }, preview }) {
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allPosts = await getAllPostsForHome(preview)
+  const allPosts = await getAllPostsForHome(preview);
   return {
     props: { allPosts, preview },
-  }
+  };
 }
